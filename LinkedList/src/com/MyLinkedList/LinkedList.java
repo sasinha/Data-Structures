434abb0daf5550ade69b8888d2a105bd4a33b91d
sasinha <sinhasaeng@gmail.com>
2016-11-09 14:31:16 -0800

    Linked List class

--- LinkedList/src/com/MyLinkedList/LinkedList.java
+++ LinkedList/src/com/MyLinkedList/LinkedList.java
@@ -1,12 +1,18 @@
+package com.MyLinkedList;
+
 /**
  * Created by ssinha on 11/9/16.
  */
 
 
 public class LinkedList {
+
+    // Initialize head and tail
     private Node head = null;
     private Node tail = null;
 
-
+    // Empty constructor
+    public LinkedList() {
+    }
 
 }
--- LinkedList/src/com/MyLinkedList/Node.java
+++ LinkedList/src/com/MyLinkedList/Node.java
@@ -1,8 +1,10 @@
+package com.MyLinkedList;
+
 /**
  * Created by ssinha on 11/9/16.
  */
 
-// Node in a linked list. <bounded type parameter> = restricts
+// com.MyLinkedList.Node in a linked list. <bounded type parameter> = restricts
 public class Node {
 
     // Value in node
